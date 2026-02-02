8e59c351de6139adb53be5adfe9e5501e93aa0ab
Azamat <azamat.esbol2007@gmail.com>
2026-02-02 17:10:44 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ practice/practice02/bolean/boolean_intro.py
@@ -0,0 +1,10 @@
+print(bool("Hello"))
+print(bool(15))
+def myFunction() :
+  return True
+
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
---
+++ practice/practice02/if-else/if_statement.py
@@ -0,0 +1,17 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a") # you will get an error
+
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
